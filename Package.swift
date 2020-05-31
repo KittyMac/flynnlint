475e309b52deaa952a0aeff350627f4de331bc8b
Rocco Bowling <rocco@chimerasw.com>
2020-05-31 17:21:04 -0400

    WIP: moving flynn over to swiftpm

---
+++ Package.swift
@@ -0,0 +1,43 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "FlynnLint",
+    products: [
+        .executable(name: "flynnlint", targets: ["flynnlint"]),
+        .library(name: "FlynnLintFramework", targets: ["FlynnLintFramework"])
+    ],
+    dependencies: [
+		.package(url: "https://github.com/KittyMac/Flynn.git", .branch("master")),
+        .package(url: "https://github.com/Carthage/Commandant.git", .upToNextMinor(from: "0.17.0")),
+        .package(url: "https://github.com/jpsim/SourceKitten.git", .upToNextMinor(from: "0.29.0")),
+        .package(url: "https://github.com/jpsim/Yams.git", from: "2.0.0"),
+        .package(url: "https://github.com/scottrhoyt/SwiftyTextTable.git", from: "0.9.0"),
+    ],
+    targets: [
+        .target(
+            name: "flynnlint",
+            dependencies: [
+                "Commandant",
+                "FlynnLintFramework",
+                "SwiftyTextTable",
+            ]
+        ),
+        .target(
+            name: "FlynnLintFramework",
+            dependencies: [
+                "SourceKittenFramework",
+                "Yams",
+            ]
+        ),
+        .testTarget(
+            name: "FlynnLintFrameworkTests",
+            dependencies: [
+                "FlynnLintFramework"
+            ],
+            exclude: [
+                "Resources",
+            ]
+        )
+    ]
+)
